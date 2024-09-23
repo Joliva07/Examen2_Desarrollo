aa5b1896b8071e8daee4764bf0f6c11463b2ac5e
Josué Oliva <78836905+Joliva07@users.noreply.github.com>
2024-09-23 16:34:00 -0600

    Primer_Push

--- app/config/env.js
+++ app/config/env.js
@@ -2,8 +2,8 @@ const env = {
     database: 'db_josue2403',
     username: 'usr_josue',
     password: '7lRTe1ZRKH4adHKbaKKKxmoUIEerT4OH',
-    host: 'dpg-crorotd2ng1s73ajj3c0-a.oregon-postgres.render.com',
-    //host: 'dpg-crorotd2ng1s73ajj3c0-a',
+    //host: 'dpg-crorotd2ng1s73ajj3c0-a.oregon-postgres.render.com',
+    host: 'dpg-crorotd2ng1s73ajj3c0-a',
     dialect: 'postgres',
     pool: {
       max: 5,
